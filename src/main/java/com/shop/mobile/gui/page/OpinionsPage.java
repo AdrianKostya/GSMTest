ca0d64607123ffe65d33610803608302db4c8055
Adrian Kostya <akostya@solvd.com>
2022-05-16 22:39:43 +0200

    hgf

--- src/main/java/com/shop/mobile/gui/page/OpinionsPage.java
+++ src/main/java/com/shop/mobile/gui/page/OpinionsPage.java
@@ -29,7 +29,7 @@ public class OpinionsPage extends Abstract {
     }
 
     //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
-    //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
+    //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
 
 //    public void isDateNewestFirst(){
 //        SimpleDateFormat allDate = new SimpleDateFormat("dd MMM yyyy hh:min aa");
