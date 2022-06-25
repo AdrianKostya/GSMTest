6ee6cbe861905d58e87e8dcce20356436e4b3137
Adrian Kostya <akostya@solvd.com>
2022-06-25 13:00:20 +0200

    /usr/bin/chromedriver/chromedriver

--- src/main/java/com/shop/mobile/utilities/Abstract.java
+++ src/main/java/com/shop/mobile/utilities/Abstract.java
@@ -20,7 +20,7 @@ public class Abstract {
 
     @BeforeTest
     public void before(){
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver/chromedriver");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
     }
